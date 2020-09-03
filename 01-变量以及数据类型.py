fb7f0652a4cc2d8e0052587409c68579b4b538c1
shenchen <572631358@qq.com>
2020-09-03 17:15:08 +0800

    Initial commit

---
+++ 01-变量以及数据类型.py
@@ -0,0 +1,19 @@
+a = 'hello world'
+print(a)
+
+print((-1) ** 0.5)  # complex类型
+
+# 布尔类型
+print(False)
+
+# 列表类型
+names = ['张三', 'liSi']
+
+# 字典类型
+person = {'name': 'zhangSan', 'age': 18}
+
+# 元组类型
+nums = (1, 23, 4)
+
+# 集合类型
+x = {1, 'test', False}
---
+++ 02-查看数据类型.py
@@ -0,0 +1,13 @@
+a = 34
+b = 'test'
+c = False
+d = ['zz']
+e = (1, 23, 4)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print(type(3.14))
